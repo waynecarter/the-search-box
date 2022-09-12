fb0ce1ad48cac530587637dc540c9afa1cd16f6b
waynecarter <wayneacarter@hotmail.com>
2022-09-12 17:01:07 -0400

    For search, use json object and match. This is to work around issues w/ special characters.

--- gateway/graphql/resolvers/Query/productsAutocomplete/code.sql
+++ gateway/graphql/resolvers/Query/productsAutocomplete/code.sql
@@ -2,7 +2,7 @@ SELECT raw(name)
 FROM   products
 WHERE  type = 'product'
        AND CASE
-             WHEN len($search) > 0 THEN SEARCH(products, $search, { "index": "autocomplete" })
+             WHEN len($search) > 0 THEN SEARCH(products, { "match": $search }, { "index": "autocomplete" })
              ELSE TRUE
            END
 LIMIT  10
